201f1183589bdca810bbf37534240424578109be
Daviti Didbaridze <ddidbaridze@gmail.com>
2021-02-14 19:24:22 +0100

    Add String Balanced Checker

---
+++ src/Expression.java
@@ -0,0 +1,42 @@
+import java.util.Stack;
+
+public class Expression {
+    String input;
+
+
+    public  Expression(String input){
+        this.input = input;
+    }
+
+    public  boolean isBalanced(){
+        Stack<Character> stack = new Stack<>();
+
+        for(char ch : input.toCharArray()){
+            if(isLeftBracket(ch)) {
+                stack.push(ch);
+            }
+            if(isRightBracket(ch)) {
+                if(stack.empty()) return false;
+                var top = stack.pop();
+                if(!bracketsMatch(top, ch))return false;
+            }
+        }
+        return  stack.empty();
+    }
+
+
+
+
+    private  boolean isLeftBracket(char ch){
+        return ch == '(' || ch  == '<' || ch == '[' || ch == '{';
+    }
+    private  boolean isRightBracket(char ch){
+        return ch == ')' || ch  == '>' || ch == ']' || ch == '}';
+    }
+    private  boolean bracketsMatch (char left, char right ){
+   return  (right == ')' && left == '(') ||
+           (right == '<' && left == '>') ||
+           (right == '{' && left == '}') ||
+           (right == '[' && left == ']');
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,12 @@ import java.util.Stack;
 
 public class Main {
     public static  void  main (String[] args){
-        String str = "abcd";
-        StringReverser reverser = new StringReverser();
-        var result  = reverser.reverse(null);
+
+  String str = "[((1 + 2 ))]";
+
+  Expression exp = new Expression(str);
+
+  var result = exp.isBalanced();
         System.out.println(result);
 
     }
